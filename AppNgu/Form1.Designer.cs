fd487de0accf5885096ebe29bcdb7f3c4754ef3b
Jarvis <triho753@gmail.com>
2025-03-01 14:40:50 +0700

    Day la commit  thu 2

--- AppNgu/Form1.Designer.cs
+++ AppNgu/Form1.Designer.cs
@@ -29,36 +29,63 @@
         private void InitializeComponent()
         {
             this.ViewSieuNgu = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.ViewSieuNgu)).BeginInit();
             this.SuspendLayout();
             // 
             // ViewSieuNgu
             // 
             this.ViewSieuNgu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.ViewSieuNgu.Location = new System.Drawing.Point(219, 136);
+            this.ViewSieuNgu.Location = new System.Drawing.Point(104, 83);
             this.ViewSieuNgu.Name = "ViewSieuNgu";
             this.ViewSieuNgu.RowHeadersWidth = 82;
             this.ViewSieuNgu.RowTemplate.Height = 33;
             this.ViewSieuNgu.Size = new System.Drawing.Size(1099, 432);
             this.ViewSieuNgu.TabIndex = 0;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(624, 560);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(150, 29);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(846, 618);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(150, 29);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1646, 923);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.ViewSieuNgu);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ViewSieuNgu)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView ViewSieuNgu;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
